79ac3b52c903848ba4dd277a31d786c8b4a6c78a
Henrietta Székely <henrietta.szekely@connatix.com>
2020-11-07 22:26:18 +0200

    week 7 updates

--- week 7 - AJAX/AJAX-examples/async.js
+++ week 7 - AJAX/AJAX-examples/async.js
@@ -1,10 +1,12 @@
 console.log("Hello There!");
 
+
 setTimeout(function () {
     console.log("Hello from set timeout!");
 }, 1000);
 
 console.log("Hello again!");
+
 
 var timeOutId = setTimeout(function () {
     console.log("Second timeout!");
--- week 7 - AJAX/AJAX-examples/fetch.js
+++ week 7 - AJAX/AJAX-examples/fetch.js
@@ -1,4 +1,4 @@
-var baseUrl = "https://jsonplaceholder.typicode.com";
+var postsBaseUrl = "https://jsonplaceholder.typicode.com";
 
 function diplayPost(post) {
   // get the post list
@@ -16,24 +16,53 @@ function diplayPost(post) {
   postList.appendChild(item);
 }
 
+function diplayGame(post) {
+  // get the post list
+  var postList = document.getElementById("postsList");
+
+  // create a list item
+  var item = document.createElement("li");
+
+  // add the post id, title and body
+  item.innerHTML = post._id + " - " + post.title + ": " + '<p>' + post.description + '</p>';
+
+  item.style.marginBottom = '20px';
+
+  // append the list item to the list
+  postList.appendChild(item);
+}
+
 // fetch only one post by id
-fetch(baseUrl + "/posts/2", { method: "GET" })
+fetch(postsBaseUrl + "/posts/102", { method: "GET" })
   .then(
     function (response) {
-      // The json() method takes a Response stream and reads it to completion.
-      // It returns a promise that resolves with the result of parsing the body text as JSON.
-      return response.json();
+      // see how the Response object looks like
+      console.log(response);
+
+      // based on the Resonse status decide what to do
+      if (response.status === 200) {
+        return response.json();
+        // It returns a promise that resolves with the result of parsing the body text as JSON.
+        // The json() method takes a Response stream and reads it to completion.
+      } else if (response.status === 404) {
+        throw new Error("Post not found!");
+      }
     }
-  ).then(
+  )
+  .then(
     function (jsonResp) {
       console.log(jsonResp);
       // call the display function
       diplayPost(jsonResp);
     }
-  );
+  ).catch(function (jsonResp) {
+    console.log(jsonResp);
+  });
 
-// fetch all posts
-fetch(baseUrl + "/posts", { method: "GET" })
+var gamesBaseUrl = "https://games-world.herokuapp.com";
+
+// fetch all games
+fetch(gamesBaseUrl + "/games", { method: "GET" })
   .then(
     function (response) {
       // parsing the response
@@ -41,21 +70,21 @@ fetch(baseUrl + "/posts", { method: "GET" })
     }
   ).then(
     function (jsonResp) {
-      /* for(var i=0; i< jsonResp.length; i++) {
-          diplayPost(jsonResp[i]);
-      }; */
+      // for(var i=0; i< jsonResp.length; i++) {
+      //   diplayPost(jsonResp[i]);
+      //};
 
       // more details about forEach - https://www.w3schools.com/jsref/jsref_foreach.asp
       jsonResp.forEach(function (element) {
-        // call the displayPost function for each element
-        diplayPost(element);
+        // call the displayGame function for each element
+        diplayGame(element);
       });
-
     }
   )
 
+
 // add a new post
-fetch(baseUrl + '/posts', {
+fetch(postsBaseUrl + '/posts', {
   method: 'POST',
   body: JSON.stringify({
     title: 'foo',
