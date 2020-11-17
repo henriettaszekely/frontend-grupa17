9496ebcfa41dcf83cda5bd99c6c3beb33b7edad1
Henrietta Székely <henrietta.szekely@connatix.com>
2020-11-17 22:13:53 +0200

    games final version

--- week 8 - AJAX - Workshop - Games/games.js
+++ week 8 - AJAX - Workshop - Games/games.js
@@ -19,25 +19,19 @@ function deleteGame(id) {
         });
 }
 
+function createGame() {
+    // get the data from the form
+    var gameData = getFormData();
 
-function getFormData() {
-    var inputsList = document.querySelectorAll("#createGameForm .form-control");
-
-    var formData = {};
-
-    for (input of inputsList) {
-        formData[input.name] = input.value;
+    // check if the data is valid, each element has value
+    if (isFormDataInvalid(gameData)) {
+        // if the for data is invalid end this function by returning from it
+        return;
     }
 
-    console.log(formData);
-    return formData;
-}
-
-
-function createGame() {
-
-    var gameData = getFormData();
+    // if the form is not invalid the function execution continues
     var formatedData = new URLSearchParams(gameData);
+    console.log("Formated from data", formatedData);
 
     fetch(baseUrl, {
         method: 'POST',
@@ -47,8 +41,8 @@ function createGame() {
         body: formatedData
     }).then(handleResponse)
         .then(displayGame)
+        .catch(displayError);
 }
-
 
 function handleResponse(response) {
     if (response.status === 404 || response.status === 400) {
@@ -65,6 +59,39 @@ function handleResponse(response) {
             return response.json();
         }
     }
+}
+
+function getFormData() {
+    var inputsList = document.querySelectorAll("#createGameForm .form-control");
+
+    var formData = {};
+
+    for (input of inputsList) {
+        formData[input.name] = input.value;
+    }
+    console.log("Form data object:", formData);
+    return formData;
+}
+
+function isFormDataInvalid(data) {
+    var inValidInputs = 0;
+    // get all the error elements
+    var errors = document.querySelectorAll("#createGameForm .error");
+    var i = 0;
+
+    for (var key in data) {
+        // iterate over each propert of the data and check if it is an empty string
+        if (data[key] === '') {
+            inValidInputs++;
+            // if an input is invalid show the it's error element
+            errors[i].style.visibility = 'visible';
+        }
+        // in each for increment the i
+        i++;
+    }
+
+    return inValidInputs > 0; // will return true if there are invalid inputs, false is the number of inputs is 0
+
 }
 
 function displayError(message) {
@@ -138,24 +165,48 @@ function hideModal() {
     modal.style.display = "none";
 }
 
+function validateInput(event) {
+    // the event object contains information about the event that called back this function
+    // event.target is the element on which the event occured
+    // check if the event.target.value is truthy, it means is not 0/null/undefined/''/false/NaN, it has an actual value
+    if (event.target.value) {
+        event.target.parentElement.parentElement.querySelector('.error').style.visibility = 'hidden';
+    } else {
+        event.target.parentElement.parentElement.querySelector('.error').style.visibility = 'visible';
+    }
+}
+
 window.addEventListener("load", function () {
     var modalSection = document.getElementById("modal");
+
+    // include the content in the modalSection
     includeHTML(modalSection).then(() => {
         var closeButtons = document.getElementsByClassName("close");
 
+        // add the click functionality to the close buttons
         for (button of closeButtons) {
-            console.log(button);
             button.onclick = hideModal;
         }
 
         var saveButton = document.getElementById("saveButton");
+        // add the click functionality to the save button
         saveButton.onclick = createGame;
 
+        var inputs = document.querySelectorAll("#createGameForm .form-control");
+        // add the validation to each input from the modal
+        for (input of inputs) {
+            // everytime something is written in the input validate it
+            input.addEventListener("keyup", validateInput)
+        }
+        // everytime the date picker is closed validate it
+        document.getElementById("gameReleaseDate").onblur = validateInput;
     });
 
+    // add the click functionality to the create button
     var createButton = document.getElementById("createButton");
     createButton.onclick = displayModal;
 
+    // fetch all games
     getGames();
 });
 
