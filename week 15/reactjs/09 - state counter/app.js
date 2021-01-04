9a04a3566079bf2c58f322f94e4d787257a491b2
Razvan Timis <timisrazvanvasile@gmail.com>
2021-01-04 21:01:57 +0200

    Add 09 Counter

---
+++ week 15/reactjs/09 - state counter/app.js
@@ -0,0 +1,40 @@
+// 9. State - Counter
+
+class CounterApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    } // valori care schimba interfata ( UI )
+
+    // this.incrementCounter = this.incrementCounter.bind(this);
+  }
+
+  incrementCounter = () => {
+    // console.log('click button');
+    const newCounter = this.state.counter + 1;
+    this.setState({ counter: newCounter });
+  }
+
+  render() {
+    console.log('counterApp - render call');
+    return (
+      <div>
+        <span style={{ fontSize: '30px', padding: '20px' }}>
+          {this.state.counter}
+        </span>
+        <button onClick={this.incrementCounter}>Increment</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<CounterApp />, document.getElementById('app'))
+
+
+
+
+
+
+
