b046d79d6edc528e50ceecbab798462bfe947730
Razvan Timis <timisrazvanvasile@gmail.com>
2021-01-04 19:42:21 +0200

    Add styling

---
+++ week 15/reactjs/05 - styling post/app.js
@@ -0,0 +1,30 @@
+// 5. Styling post 
+function Post(props) {
+  return (
+    <div className="post">
+      <h2>{props.title}</h2>
+      <p>{props.content}</p>
+    </div>
+  )
+}
+
+// Atributele se trimit intr-un obiect props la functia de la sus
+let App = (
+  <div>
+    <Post title="Post 1" content="Continut 1" />
+    <Post title="Post 2" content="Continut 2" />
+    <Post title="Post 3" content="Continut 3" />
+    <Post title="Invat React" content="Continut 4" />
+  </div>
+);
+
+
+
+ReactDOM.render(App, document.getElementById('app'))
+
+
+
+
+
+
+
