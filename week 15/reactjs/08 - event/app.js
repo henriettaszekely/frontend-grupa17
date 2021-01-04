ed28e2ce9a1afbd02baab13f67bfeae7c4e1a979
Razvan Timis <timisrazvanvasile@gmail.com>
2021-01-04 20:46:43 +0200

    Add 08 event

---
+++ week 15/reactjs/08 - event/app.js
@@ -0,0 +1,44 @@
+// 8. Event
+// Sa adaugam un event de on click pe Post
+// In DOM un div are "onclick". Ex: document.createElement('div').onclick = function(){}
+class Post extends React.Component {
+  render() {
+    // document.addEventListener('scroll', function(){
+
+    // })
+    console.log(this); // this1
+    return (
+      <div onClick={() => {
+        // this == this1
+        this.props.onPostClick();
+      }}>
+        <h2>{this.props.title}</h2>
+        <p>{this.props.content}</p>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  onPostClick() {
+    console.log('post click');
+  }
+
+  render() {
+    return (
+      <div>
+        <Post title="Invat React" content="Continut 4" onPostClick={this.onPostClick} />
+        <Post title="Post 2" content="Continut 2" onPostClick={this.onPostClick} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
+
+
+
+
+
+
+
