51b685736b75eec80a84322a343637d1060d2a4b
Razvan Timis <timisrazvanvasile@gmail.com>
2021-01-06 20:31:51 +0200

    Add

---
+++ week 15/js ecosystem/ex2/app.js
@@ -0,0 +1,4 @@
+import { sum, user } from './utils.js'
+
+console.log(sum([1, 2, 3]));
+console.log(user);
---
+++ week 15/js ecosystem/ex2/utils.js
@@ -0,0 +1,5 @@
+export function sum(array) {
+  return array.reduce((acc, el) => acc + el, 0)
+}
+
+export const user = { name: 'Razvan' }
