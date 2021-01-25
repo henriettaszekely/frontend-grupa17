e6690c9e892b6a723dff7b0a3e5843f15142871d
Razvan Timis <timisrazvanvasile@gmail.com>
2021-01-25 20:56:38 +0200

    Add

---
+++ week17/task.js
@@ -0,0 +1,22 @@
+//  Functionalitate search
+// Scopul este sa impartim task-ul in pasi mai mici
+// 1. Un sigur input cu title si de button (html)
+// 2. Afisam textul din input daca apasam butonul de search
+// 3. Implementam filtrare cu call la BE
+
+// Se ocupa de cautare pe backend ( doar face requestul)
+// returneaza o lista de filme
+// Ex: filters = { title: 'test'}
+// Nu afiseaza nimic in UI ( nu folose document.createElement)
+function searchMoviesApi(filters) {
+
+  return [
+    { title: 'test1' }
+  ]; // sa returnezi date mock
+}
+
+function showUI() {
+  // adaugam event de click pe buton
+  // trebuie sa facem obiect filters
+  // apelam searchMoviesApi cu parametru filters daca sa dat click
+}
